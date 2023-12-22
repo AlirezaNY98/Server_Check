b816dffc361d257010c874afbeab88b3a2801679
AliReza <107261014+AlirezaNY98@users.noreply.github.com>
2023-12-22 11:47:43 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,9 +20,19 @@ sed -i "s/server_name  localhost;/server_name  ${NGINX_DOMAIN} www.${NGINX_DOMAI
 
 nginx -s reload
 
-apt install certbot python3-certbot-nginx -y
+read -p "Do you set domains A record?(y/n)" DOMAIN_AN
 
-certbot --nginx -d $NGINX_DOMAIN -d www.$NGINX_DOMAIN
+if [ $DOMAIN_AN = "y" ]; then
+
+  apt install certbot python3-certbot-nginx -y
+
+  certbot --nginx -d $NGINX_DOMAIN -d www.$NGINX_DOMAIN
+
+  echo "all things set up you can check your domain and then write exit for delete nginx docker."
+else
+  echo "first set records and then try this script again."
+fi
+
 
 
 

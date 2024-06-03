6d43da61f27dddc4e02e72a95a1f26b18e2c93cf
AliReza <107261014+AlirezaNY98@users.noreply.github.com>
2024-06-03 17:33:54 +0330

    update

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # server check
 
-You can check your server and make sure it is useful in all countries and the IP is white.
+For start run the below command:
 
-This bash script first of all installs Docker, Python, and other useful dependencies.
+```bash
 
-Then you should write your domain, and the system will start a removable nginx (nginx-server-test) and set your domain in nginx. After that, you should go to your registrar and set a record for your server IP in @ and www.
+cd /tmp && git clone "https://github.com/AlirezaNY98/server-check.git" && bash start.sh
 
-Then the script asks you about record setting, and if you set the record, you can answer yes and SSL will be generated for your domains.
-
-After that, you can go to this site (https://check-host.net/) and write down your domain and check your server.
-
-If you like this script, give it a big star.
+```
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-read -p "do you want install basic server configurations(docker, python, ...)?(y/n)" CONFIG
+read -p "Do you want install basic server configurations(docker, python, ...)?(y/n)" CONFIG
 
 if [ $CONFIG = "y" ]; then
   git clone "https://github.com/mohammad76/config-server" /home/server-config/ && cd /home/server-config/ && bash config.sh
@@ -17,5 +17,5 @@ docker exec -it nginx-server-test /bin/bash -c "apt update -y \
 && sed -i \"s/server_name  localhost;/server_name  ${NGINX_DOMAIN} www.${NGINX_DOMAIN};/\" /etc/nginx/conf.d/default.conf \
 && nginx -s reload \
 && apt install certbot python3-certbot-nginx -y \
-&& certbot certonly --manual --preferred-challenges dns -d $NGINX_DOMAIN -d www.$NGINX_DOMAIN \
+&& certbot --nginx -d $NGINX_DOMAIN -d www.$NGINX_DOMAIN \
 && echo \"all things set up you can check your domain and then write exit for delete nginx docker.\""
